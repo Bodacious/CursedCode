1af820b12645e25ad0e9e09a430e56735a0bf45e
Bodacious <gavin@gavinmorrice.com>
2024-05-07 19:45:37 +0100

    Refactor faraday connection to use middleware

--- acme/base.rb
+++ acme/base.rb
@@ -3,8 +3,6 @@ module Acme
   class Base
     require "faraday"
 
-    class Error < StandardError; end
-
     API_BASE_URL = "https://dummyjson.com/".freeze
 
     def get(path, **params)
@@ -25,14 +23,17 @@ module Acme
 
     private
 
+    def faraday_connection
+      @faraday_connection ||= Faraday.new(API_BASE_URL) do |config|
+        config.request(:json)
+        config.response(:json)
+      end
+    end
+
     def make_request(verb, path, **params_or_data)
       uri = URI.join(API_BASE_URL, path)
-      response = Faraday.public_send(verb, uri, **params_or_data)
-      if response.success?
-        JSON.parse(response.body)
-      else
-        raise Error, response.body
-      end
+      response = faraday_connection.public_send(verb, uri, **params_or_data)
+      response.body
     end
   end
 end
