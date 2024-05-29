7a082051ce257e5bb0f24b691e4491dac6ea4a5a
Bodacious <gavin@gavinmorrice.com>
2024-05-29 19:11:32 +0100

    Add after directory for refactoring

---
+++ base_class_stripe/after/stripe/customer.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+require_relative 'base'
+
+module Stripe
+  class Customer < Base
+    CUSTOMER_BASE_URL = "#{BASE_URL}/v1/customers".freeze
+
+    def list
+      response = get(CUSTOMER_BASE_URL)
+
+      JSON.parse(response)
+    end
+
+    def create(attributes)
+      response = post(CUSTOMER_BASE_URL, attributes)
+
+      JSON.parse(response)
+    end
+
+    def fetch_one(id)
+      endpoint = [CUSTOMER_BASE_URL, id].join('/')
+
+      response = get(endpoint)
+
+      JSON.parse(response)
+    end
+
+    def patch(id, attributes)
+      endpoint = [CUSTOMER_BASE_URL, id].join('/')
+
+      response = super(endpoint, attributes)
+
+      JSON.parse(response)
+    end
+
+    def delete(id)
+      endpoint = [CUSTOMER_BASE_URL, id].join('/')
+
+      response = super(endpoint)
+
+      JSON.parse(response)
+    end
+
+    def search(query)
+      endpoint = "#{CUSTOMER_BASE_URL}?query:#{query}"
+
+      response = get(endpoint)
+
+      JSON.parse(response)
+    end
+  end
+end
